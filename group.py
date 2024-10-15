bfee963529cfb810a3e7dec45c7febd8b9f34e7e
dp0768 <ucapend@ucl.ac.uk>
2024-10-15 18:38:15 +0100

    Summary Data functions for my friend group data structure

--- group.py
+++ group.py
@@ -9,7 +9,7 @@ my_group = {
     },
     'Zalika' : 
     {
-        'Age' : 26,
+        'Age' : 28,
         'Job' : ['Artist'],
         'Connections' : {'Friend' : 'Jill'}
     },
@@ -28,7 +28,33 @@ my_group = {
     
 }
 
-print(my_group)
+### Issue 7 - Summary data from group ###
+def max_age(my_group=my_group,ages=[]):
+    ages = []
+    for name in my_group:
+        ages.append(my_group[name]['Age'])
+    return(max(ages))
+
+def mean_relations(my_group=my_group,n_relations=0):
+    for name in my_group:
+        n_relations += len(my_group[name]['Connections'])
+    return n_relations/len(name)
+
+def max_age_with_relation(my_group=my_group,ages=[]):
+    ages = []
+    for name in my_group:
+        if len(my_group[name]['Connections']) > 0:
+            ages.append(my_group[name]['Age'])
+    return(max(ages))
+
+def max_age_with_friend(my_group=my_group,ages=[]):
+    for name in my_group: 
+        if 'Friend' in list(my_group[name]['Connections'].keys()):
+            ages.append(my_group[name]['Age'])
+    return max(ages)
 
 
-
+print(max_age())
+print(mean_relations())
+print(max_age_with_relation())
+print(max_age_with_friend())
